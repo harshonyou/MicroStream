8ab72e542c82a4d626bf6e21a6ba2eb90a581da5
Harsh Mohan <harshmohan813@gmail.com>
2024-01-01 22:31:34 +0000

    Create VideoFeedbackEventDTOSerdeTest.java

---
+++ trending-tag-service/src/test/java/com/example/serde/VideoFeedbackEventDTOSerdeTest.java
@@ -0,0 +1,30 @@
+package com.example.serde;
+
+import com.example.dto.VideoFeedbackEventDTO;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class VideoFeedbackEventDTOSerdeTest {
+    @Inject
+    VideoFeedbackEventDTOSerde videoFeedbackEventDTOSerde;
+
+    @Test
+    public void testSerializeAndDeserialize() {
+        VideoFeedbackEventDTO originalDto = new VideoFeedbackEventDTO();
+        originalDto.setTags(Set.of("exampleTag1", "exampleTag2"));
+        originalDto.setLikeStatus(true);
+
+        byte[] serializedData = videoFeedbackEventDTOSerde.serialize("topic", originalDto);
+
+        VideoFeedbackEventDTO deserializedDto = videoFeedbackEventDTOSerde.deserialize("topic", serializedData);
+
+        assertEquals(originalDto.getTags(), deserializedDto.getTags());
+        assertEquals(originalDto.isLikeStatus(), deserializedDto.isLikeStatus());
+    }
+}
