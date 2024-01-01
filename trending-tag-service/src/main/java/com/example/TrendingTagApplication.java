e4670b9a2e450647590d95cea49cca017dc506e6
Harsh Mohan <harshmohan813@gmail.com>
2024-01-01 22:27:22 +0000

    Rename TrendingTagApplication

--- trending-tag-service/src/main/java/com/example/TrendingTagApplication.java
+++ trending-tag-service/src/main/java/com/example/TrendingTagApplication.java
@@ -2,9 +2,9 @@ package com.example;
 
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
+public class TrendingTagApplication {
 
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        Micronaut.run(TrendingTagApplication.class, args);
     }
 }
