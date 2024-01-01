b4bc06b4a578a2d97b80ab6f2d7a65f2515f1b5d
Harsh Mohan <harshmohan813@gmail.com>
2024-01-01 22:30:26 +0000

    Update AggregatedTagLikeRepository.java

--- trending-tag-service/src/main/java/com/example/repository/AggregatedTagLikeRepository.java
+++ trending-tag-service/src/main/java/com/example/repository/AggregatedTagLikeRepository.java
@@ -16,33 +16,34 @@ import static com.example.util.IntervalConverter.convertToPostgresInterval;
 @JdbcRepository(dialect = Dialect.POSTGRES)
 public interface AggregatedTagLikeRepository extends CrudRepository<AggregatedTagLike, String> {
     @Query(value = "WITH current_hour_aggregated AS ( " +
-            "    SELECT tag, " +
-            "           SUM(aggregated_likes) AS hourly_likes, " +
-            "           DATE_TRUNC('hour', minute_interval) AS hour_interval " +
-            "    FROM aggregated_tag_likes " +
-            "    WHERE minute_interval >= DATE_TRUNC('hour', NOW()) " +
-            "      AND minute_interval < DATE_TRUNC('hour', NOW()) + INTERVAL '1 hour' " +
-            "    GROUP BY tag, hour_interval " +
-            ") " +
-            "SELECT tag, hourly_likes " +
-            "FROM current_hour_aggregated " +
-            "ORDER BY hourly_likes DESC " +
-            "LIMIT :limit", nativeQuery = true)
+                    "    SELECT tag, " +
+                    "           SUM(aggregated_likes) AS hourly_likes, " +
+                    "           DATE_TRUNC('hour', minute_interval) AS hour_interval " +
+                    "    FROM aggregated_tag_likes " +
+                    "    WHERE minute_interval >= DATE_TRUNC('hour', NOW()) " +
+                    "      AND minute_interval < DATE_TRUNC('hour', NOW()) + INTERVAL '1 hour' " +
+                    "    GROUP BY tag, hour_interval " +
+                    ") " +
+                    "SELECT tag, hourly_likes " +
+                    "FROM current_hour_aggregated " +
+                    "ORDER BY hourly_likes DESC " +
+                    "LIMIT :limit",
+            nativeQuery = true)
     List<CurrentHourAggregatedTagLikeDTO> findTopTagsByHourlyLikes(Integer limit);
 
     @Query(value = "WITH current_interval_aggregated AS ( " +
-            "    SELECT tag, " +
-            "           SUM(aggregated_likes) AS interval_likes, " +
-            "           DATE_TRUNC('hour', minute_interval) AS start_interval " +
-            "    FROM aggregated_tag_likes " +
-            "    WHERE minute_interval >= NOW() - CAST(:interval AS interval) " +
-            "      AND minute_interval < NOW() " +
-            "    GROUP BY tag, start_interval " +
-            ") " +
-            "SELECT tag, interval_likes " +
-            "FROM current_interval_aggregated " +
-            "ORDER BY interval_likes DESC " +
-            "LIMIT :limit", nativeQuery = true)
+                    "    SELECT tag, " +
+                    "           SUM(aggregated_likes) AS interval_likes " +
+                    "    FROM aggregated_tag_likes " +
+                    "    WHERE minute_interval >= NOW() - CAST(:interval AS interval) " +
+                    "      AND minute_interval < NOW() " +
+                    "    GROUP BY tag " +
+                    ")  " +
+                    "SELECT tag, interval_likes " +
+                    "FROM current_interval_aggregated " +
+                    "ORDER BY interval_likes DESC " +
+                    "LIMIT :limit",
+            nativeQuery = true)
     List<PastIntervalAggregatedTagLikeDTO> findTopTagsByIntervalLikes(String interval, Integer limit);
 
     default List<PastIntervalAggregatedTagLikeDTO> findTopTagsByCustomInterval(String interval, Integer limit) {
