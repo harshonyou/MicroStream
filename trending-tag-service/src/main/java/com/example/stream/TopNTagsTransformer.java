7bd135c225674bf1b6be6be23213f36e8850a16d
Harsh Mohan <harshmohan813@gmail.com>
2024-01-02 13:37:16 +0000

    Fix TopNTagsTransformer

--- trending-tag-service/src/main/java/com/example/stream/TopNTagsTransformer.java
+++ trending-tag-service/src/main/java/com/example/stream/TopNTagsTransformer.java
@@ -40,7 +40,7 @@ public class TopNTagsTransformer implements Transformer<Windowed<String>, Long, 
 
     @Override
     public KeyValue<String, Long> transform(Windowed<String> key, Long value) {
-        if (topNTags.size() > N) {
+        if (topNTags.size() >= N) {
             if (value > topNTags.peek().getValue()) {
                 topNTags.add(new AbstractMap.SimpleEntry<>(key.key(), value));
                 topNTags.poll();
