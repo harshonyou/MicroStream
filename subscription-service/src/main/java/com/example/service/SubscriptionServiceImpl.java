192d14cb4e18df998f13d392290601166448a3bd
Harsh Mohan <harshmohan813@gmail.com>
2024-01-03 00:58:19 +0000

    Add constructor init (SubscriptionServiceImpl)

--- subscription-service/src/main/java/com/example/service/SubscriptionServiceImpl.java
+++ subscription-service/src/main/java/com/example/service/SubscriptionServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.dto.TagEngagementEventDTO;
 import com.example.producer.TagSubscriptionEventClient;
+import com.example.repository.Neo4jTagRepository;
 import com.example.repository.TagRepository;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -9,11 +10,14 @@ import jakarta.inject.Singleton;
 @Singleton
 public class SubscriptionServiceImpl implements SubscriptionService {
 
-    @Inject
-    private TagRepository tagRepository;
+    private final TagRepository tagRepository;
 
-    @Inject
-    TagSubscriptionEventClient eventClient;
+    private final TagSubscriptionEventClient eventClient;
+    
+    public SubscriptionServiceImpl(Neo4jTagRepository tagRepository, TagSubscriptionEventClient eventClient) {
+        this.tagRepository = tagRepository;
+        this.eventClient = eventClient;
+    }
 
     @Override
     public void subscribeUserToTag(String tagName, String userId) {
