d2df65080a1d47e793db017f779b68ef87606c42
Harsh Mohan <harshmohan813@gmail.com>
2024-01-03 01:01:24 +0000

    Create VideoServiceImplTest.java

---
+++ subscription-service/src/test/java/com/example/service/VideoServiceImplTest.java
@@ -0,0 +1,111 @@
+package com.example.service;
+
+import com.example.dto.TagDTO;
+import com.example.dto.VideoDTO;
+import com.example.mapper.TagMapper;
+import com.example.model.User;
+import com.example.model.Video;
+import com.example.repository.Neo4jTagRepository;
+import com.example.repository.Neo4jUserRepository;
+import com.example.repository.Neo4jVideoRepository;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.Duration;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@MicronautTest(transactional = false, environments = "no-streams")
+class VideoServiceImplTest {
+    @Inject
+    Neo4jVideoRepository videoRepository;
+
+    @Inject
+    Neo4jTagRepository tagRepository;
+
+    @Inject
+    Neo4jUserRepository userRepository;
+
+    @Inject
+    VideoServiceImpl videoService;
+
+    @Mock
+    Neo4jVideoRepository mockVideoRepository;
+
+    @AfterEach
+    public void tearDown() {
+        videoRepository.deleteAll();
+        tagRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void testPostVideo() {
+        UUID videoId = UUID.fromString("00000000-0000-0000-0000-000000000000");
+        String videoName = "video-name";
+        Long videoViews = 0L;
+        String userId = "user-id";
+        String userName = "user-name";
+        Set<TagDTO> tags = Set.of(new TagDTO("tag-name"));
+        userRepository.addUser(new User(userId, userName));
+        tagRepository.addTag(TagMapper.fromDTO(tags.iterator().next()));
+        videoService.postVideo(userId, new VideoDTO(videoId, videoName, videoViews), tags);
+
+        Optional<Video> video = videoRepository.findById(videoId);
+        assertTrue(video.isPresent());
+        assertEquals(videoId, video.get().getId());
+        assertEquals(videoName, video.get().getTitle());
+        assertEquals(videoViews, video.get().getViews());
+    }
+
+    @Test
+    public void testWatchVideo() {
+        UUID videoId = UUID.fromString("00000000-0000-0000-0000-000000000000");
+        String videoName = "video-name";
+        Long videoViews = 0L;
+        String userId = "user-id";
+        String userName = "user-name";
+        Set<TagDTO> tags = Set.of(new TagDTO("tag-name"));
+        userRepository.addUser(new User(userId, userName));
+        tagRepository.addTag(TagMapper.fromDTO(tags.iterator().next()));
+        videoService.postVideo(userId, new VideoDTO(videoId, videoName, videoViews), tags);
+        videoService.watchVideo(videoId, userId);
+
+        Optional<Video> video = videoRepository.findById(videoId);
+        assertTrue(video.isPresent());
+        assertEquals(videoId, video.get().getId());
+        assertEquals(videoName, video.get().getTitle());
+        assertEquals(videoViews + 1, video.get().getViews());
+    }
+
+    @Test
+    public void testLikeVideo() {
+        MockitoAnnotations.openMocks(this);
+
+        VideoServiceImpl mockService = new VideoServiceImpl(mockVideoRepository);
+
+        UUID videoId = UUID.fromString("00000000-0000-0000-0000-000000000000");
+        String videoName = "video-name";
+        Long videoViews = 0L;
+        String userId = "user-id";
+        String userName = "user-name";
+        Set<TagDTO> tags = Set.of(new TagDTO("tag-name"));
+        userRepository.addUser(new User(userId, userName));
+        tagRepository.addTag(TagMapper.fromDTO(tags.iterator().next()));
+        mockService.postVideo(userId, new VideoDTO(videoId, videoName, videoViews), tags);
+        mockService.likeVideo(videoId, userId);
+
+        verify(mockVideoRepository).postVideo(eq(userId), any(Video.class), any(Set.class));
+        verify(mockVideoRepository).likeVideo(eq(videoId), eq(userId));
+    }
+}
