295a36142ab6dc1258bd915bcf8ea5429cadf805
Harsh Mohan <harshmohan813@gmail.com>
2024-01-03 00:57:29 +0000

    Made UserRepository an interface

--- subscription-service/src/main/java/com/example/repository/UserRepository.java
+++ subscription-service/src/main/java/com/example/repository/UserRepository.java
@@ -1,62 +1,11 @@
 package com.example.repository;
 
-import com.example.dto.RecommendedVideoDTO;
 import com.example.model.User;
-import jakarta.inject.Singleton;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.Session;
-import org.neo4j.driver.Transaction;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
-@Singleton
-public class UserRepository { // TODO: Have an interface
-    private final Driver driver;
+public interface UserRepository {
+    Optional<User> findById(String userId);
 
-    UserRepository(Driver driver) {
-        this.driver = driver;
-    }
-
-    public Optional<User> findById(String userId) {
-        try (Session session = driver.session()) {
-            return session.readTransaction(tx -> findById(tx, userId));
-        }
-    }
-
-    private Optional<User> findById(Transaction tx, String userId) {
-        String query = """
-                MATCH (u:User {id: $userId})
-                RETURN u
-                """;
-
-        var result = tx.run(query, org.neo4j.driver.Values.parameters("userId", userId));
-
-        if (result.hasNext()) {
-            var record = result.single();
-            String userName = record.get("u").get("name").asString();
-            return Optional.of(new User(userId, userName));
-        } else {
-            return Optional.empty();
-        }
-    }
-
-    public void addUser(User user) {
-        try (Session session = driver.session()) {
-            session.writeTransaction(tx -> createUser(tx, user));
-        }
-    }
-
-    private Void createUser(Transaction tx, User user) {
-        String query = """
-                CREATE (u:User {id: $userId, name: $name})
-                """;
-        tx.run(query, org.neo4j.driver.Values.parameters(
-                "userId", user.getId(),
-                "name", user.getName()));
-
-        return null;
-    }
-
+    void addUser(User user);
 }
