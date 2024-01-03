11c0507e960f0a60a88f277175ff76b62e957d20
Harsh Mohan <harshmohan813@gmail.com>
2024-01-03 01:01:40 +0000

    Create SubscriptionControllerTest.java

---
+++ subscription-service/src/test/java/com/example/controller/SubscriptionControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+import com.example.service.SubscriptionService;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@MicronautTest(environments = "no-streams")
+class SubscriptionControllerTest {
+    @Mock
+    SubscriptionService mockSubscriptionService;
+
+    SubscriptionController subscriptionController;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        subscriptionController = new SubscriptionController(mockSubscriptionService);
+    }
+
+    @Test
+    public void testSubscribeUserToTag() {
+        assertEquals(HttpStatus.OK, subscriptionController.subscribeUserToTag("123", "action").getStatus());
+    }
+
+    @Test
+    public void testUnsubscribeUserFromTag() {
+        assertEquals(HttpStatus.OK, subscriptionController.unsubscribeUserFromTag("123", "action").getStatus());
+    }
+}
