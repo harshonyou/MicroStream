b0ba9edcfb6461a3a53f7d7007d8b76df99913d4
Harsh Mohan <harshmohan813@gmail.com>
2024-01-03 01:01:17 +0000

    Create UserServiceImplTest.java

---
+++ subscription-service/src/test/java/com/example/service/UserServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.example.service;
+
+import com.example.dto.UserDTO;
+import com.example.model.User;
+import com.example.repository.Neo4jUserRepository;
+import com.example.repository.UserRepository;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(transactional = false, environments = "no-streams")
+class UserServiceImplTest {
+    @Inject
+    Neo4jUserRepository userRepository;
+
+    @Inject
+    UserServiceImpl userService;
+
+    @AfterEach
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void testFindByIdWithNonExistingId() {
+        assertFalse(userService.findById("non-existing-id").isPresent());
+    }
+
+    @Test
+    public void testAddUser() {
+        String userId = "user-id";
+        String userName = "user-name";
+        userService.addUser(userId, userName);
+
+        Optional<UserDTO> user = userService.findById(userId);
+        assertTrue(user.isPresent());
+        assertEquals(userId, user.get().getId());
+        assertEquals(userName, user.get().getName());
+    }
+}
