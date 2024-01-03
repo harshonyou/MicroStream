96cf3fb4ab5ce02d65a6733aedd83c49033956de
Harsh Mohan <harshmohan813@gmail.com>
2024-01-03 00:56:24 +0000

    Create Neo4jTagRepository.java

---
+++ subscription-service/src/main/java/com/example/repository/Neo4jTagRepository.java
@@ -0,0 +1,108 @@
+package com.example.repository;
+
+import com.example.model.Tag;
+import jakarta.inject.Singleton;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+
+import java.util.Optional;
+
+
+@Singleton
+public class Neo4jTagRepository implements TagRepository {
+    private final Driver driver;
+
+    Neo4jTagRepository(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Optional<Tag> findByTagName(String tagName) {
+        try (Session session = driver.session()) {
+            return session.readTransaction(tx -> findByTagName(tx, tagName));
+        }
+    }
+
+    private Optional<Tag> findByTagName(Transaction tx, String tagName) {
+        String query =  """
+                        MATCH (t:Tag {name: $tagName})
+                        RETURN t
+                        """;
+
+        var result = tx.run(query, org.neo4j.driver.Values.parameters("tagName", tagName));
+
+        if (result.hasNext()) {
+            var record = result.single();
+            return Optional.of(new Tag(tagName));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public void addTag(Tag tag) {
+        try (Session session = driver.session()) {
+            session.writeTransaction(tx -> addTag(tx, tag));
+        }
+    }
+
+    private Void addTag(Transaction tx, Tag tag) {
+        String query =  """
+                        CREATE (t:Tag {name: $tagName})
+                        """;
+        tx.run(query, org.neo4j.driver.Values.parameters(
+                "tagName", tag.getName()));
+
+        return null;
+    }
+
+    public void associateUserWithTag(String tagName, String userId) {
+        try (Session session = driver.session()) {
+            session.writeTransaction(tx -> associateUserWithTag(tx, tagName, userId));
+        }
+    }
+
+    private Void associateUserWithTag(Transaction tx, String tagName, String userId) {
+        String query =  """
+                        MATCH (u:User {id: $userId}), (t:Tag {name: $tagName})
+                        MERGE (u)-[:SUBSCRIBES_TO]->(t)
+                        """;
+
+        tx.run(query, org.neo4j.driver.Values.parameters(
+                "userId", userId,
+                "tagName", tagName));
+        return null;
+    }
+
+    public void disassociateUserFromTag(String tagName, String userId) {
+        try (Session session = driver.session()) {
+            session.writeTransaction(tx -> disassociateUserFromTag(tx, tagName, userId));
+        }
+    }
+
+    private Void disassociateUserFromTag(Transaction tx, String tagName, String userId) {
+        String query =  """
+                        MATCH (u:User {id: $userId})-[r:SUBSCRIBES_TO]->(t:Tag {name: $tagName})
+                        DELETE r
+                        """;
+
+        tx.run(query, org.neo4j.driver.Values.parameters(
+                "userId", userId,
+                "tagName", tagName));
+        return null;
+    }
+
+    public void deleteAll() {
+        try (Session session = driver.session()) {
+            session.writeTransaction(this::deleteAllTags);
+        }
+    }
+
+    private Void deleteAllTags(Transaction tx) {
+        String query =  """
+                        MATCH (t:Tag)
+                        DETACH DELETE t
+                        """;
+        tx.run(query);
+        return null;
+    }
+}
