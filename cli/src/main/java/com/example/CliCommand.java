cfb250eb9584775d4131cb747b23759588c4828f
Harsh Mohan <harshmohan813@gmail.com>
2024-01-04 22:28:19 +0000

    Update CliCommand.java

--- cli/src/main/java/com/example/CliCommand.java
+++ cli/src/main/java/com/example/CliCommand.java
@@ -1,18 +1,35 @@
 package com.example;
 
+import com.example.engagement.WatchVideo;
+import com.example.feedback.DislikeVideo;
+import com.example.feedback.LikeVideo;
+import com.example.fetch.SuggestedVideos;
+import com.example.fetch.UserProfile;
+import com.example.fetch.UserTimeline;
 import com.example.post.PostVideo;
+import com.example.tag.SubscribeTag;
+import com.example.tag.UnsubscribeTag;
+import com.example.trend.CurrentTopTag;
+import com.example.trend.PastTopTag;
 import io.micronaut.configuration.picocli.PicocliRunner;
-import io.micronaut.context.ApplicationContext;
 
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
 @Command(name = "cli", description = "...",
-        mixinStandardHelpOptions = true, subcommands = {
-                PostVideo.class
-        })
+    mixinStandardHelpOptions = true, subcommands = {
+        PostVideo.class,
+        UserProfile.class,
+        WatchVideo.class,
+        LikeVideo.class,
+        DislikeVideo.class,
+        CurrentTopTag.class,
+        PastTopTag.class,
+        SubscribeTag.class,
+        UnsubscribeTag.class,
+        SuggestedVideos.class,
+        UserTimeline.class
+    })
 public class CliCommand implements Runnable {
 
     @Option(names = {"-v", "--verbose"}, description = "...")
