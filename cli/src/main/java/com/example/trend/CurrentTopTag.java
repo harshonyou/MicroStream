058b2dfcfca07a6841fec146b5f8741695432ac7
Harsh Mohan <harshmohan813@gmail.com>
2024-01-04 22:28:05 +0000

    Add CurrentTopTag

---
+++ cli/src/main/java/com/example/trend/CurrentTopTag.java
@@ -0,0 +1,54 @@
+package com.example.trend;
+
+// CommandLine.Command to get the current hour window top trending tags
+// Takes in args for the limit of tags to return and use TrendingTagServiceHttpClient getCurrentTopHashtags()
+
+import com.example.api.TrendingTagServiceHttpClient;
+import com.example.dto.CurrentHourAggregatedTagLikeDTO;
+import com.example.dto.VideoDTO;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import jakarta.inject.Inject;
+import picocli.CommandLine;
+
+import java.util.List;
+
+@CommandLine.Command(
+        name = "current-top",
+        description = "Get the current hour window [from 0th minute to 59th minute] top trending tags"
+)
+public class CurrentTopTag implements Runnable {
+    @CommandLine.Option(
+            names = {"-l", "--limit"},
+            description = "Limit of tags to return",
+            defaultValue = "10"
+    )
+    private Integer limit;
+
+    @CommandLine.Option(names = {"--verbose"}, description = "Verbose mode")
+    private boolean verbose;
+
+    @Inject
+    TrendingTagServiceHttpClient trendingTagServiceClient;
+
+    @Override
+    public void run() {
+        if (verbose) {
+            System.out.println("Limit: " + limit);
+        }
+
+        HttpResponse<List<CurrentHourAggregatedTagLikeDTO>> response = trendingTagServiceClient.getCurrentTopTags(limit);
+
+        if(response.getStatus() == HttpStatus.NO_CONTENT) {
+            System.out.println("No tags found");
+        } else {
+            System.out.println("Current top tags in the current hour window [from 0th minute to 59th minute]");
+            response.body().stream()
+                    .map(CurrentHourAggregatedTagLikeDTO::formatCurrentHourAggregatedTagLikeDTO)
+                    .forEach(System.out::println);
+        }
+
+        if(verbose) System.out.println(response.body());
+    }
+}
+
---
+++ cli/src/test/java/com/example/trend/CurrentTopTagTest.java
@@ -0,0 +1,28 @@
+package com.example.trend;
+
+import com.example.CliCommand;
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.Environment;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrentTopTagTest {
+    @Test
+    public void testWithCommandLineOption() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(baos));
+
+        try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+            String[] args = new String[]{"current-top", "-l", "10", "--verbose"};
+            PicocliRunner.run(CliCommand.class, ctx, args);
+            out.println(baos);
+            // cli
+        }
+    }
+}
