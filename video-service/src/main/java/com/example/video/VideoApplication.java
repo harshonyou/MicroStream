b3e88bbe35efc97235d8164418ed97a919209c50
Harsh Mohan <harshmohan813@gmail.com>
2024-01-07 22:46:20 +0000

    Update VideoApplication.java

--- video-service/src/main/java/com/example/video/VideoApplication.java
+++ video-service/src/main/java/com/example/video/VideoApplication.java
@@ -2,9 +2,9 @@ package com.example.video;
 
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
+public class VideoApplication {
 
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        Micronaut.run(VideoApplication.class, args);
     }
 }
