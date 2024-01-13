b6d927c03f5e8f976d8b30526326ba65e69deb92
Harsh Mohan <harshmohan813@gmail.com>
2024-01-13 02:24:08 +0000

    Add variable for cassandra migration

--- video-service/src/main/java/com/example/ApplicationStartup.java
+++ video-service/src/main/java/com/example/ApplicationStartup.java
@@ -2,6 +2,7 @@ package com.example;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.example.repository.*;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.discovery.event.ServiceReadyEvent;
 import jakarta.inject.Singleton;
@@ -13,19 +14,29 @@ public class ApplicationStartup implements ApplicationEventListener<ServiceReady
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationStartup.class);
 
     private final CqlSession cqlSession;
+    private final boolean migration;
 
-    public ApplicationStartup(CqlSession cqlSession) {
+    public ApplicationStartup(
+            CqlSession cqlSession,
+            @Property(name = "cassandra.migration") boolean migration) {
         this.cqlSession = cqlSession;
+        this.migration = migration;
     }
 
     public void onApplicationEvent(ServiceReadyEvent event) {
         LOGGER.info("Startup Initialization");
-        CassandraVideoRepository.createTableVideo(cqlSession);
-        LOGGER.info("+ Table VideoItems created if needed.");
-        CassandraVideoEngagementRepository.createTableUserVideoWatch(cqlSession);
-        LOGGER.info("+ Table UserVideoWatch created if needed.");
-        CassandraVideoTagRepository.createTableTag(cqlSession);
-        LOGGER.info("+ Table VideoByHashtag created if needed.");
+
+        if(migration) {
+            CassandraVideoRepository.createTableVideo(cqlSession);
+            LOGGER.info("+ Table VideoItems created if needed.");
+            CassandraVideoEngagementRepository.createTableUserVideoWatch(cqlSession);
+            LOGGER.info("+ Table UserVideoWatch created if needed.");
+            CassandraVideoTagRepository.createTableTag(cqlSession);
+            LOGGER.info("+ Table VideoByHashtag created if needed.");
+        } else {
+            LOGGER.info("+ Skipping table creation.");
+        }
+
         LOGGER.info("[OK]");
     }
 }
