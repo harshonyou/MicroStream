39a3fb5a7f6e2668205563f43c87f1e7e6dcc59c
Harsh Mohan <harshmohan813@gmail.com>
2024-01-17 02:09:05 +0000

    Add Health Checks

---
+++ trending-tag-service/src/main/java/com/example/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/api/v1")
+public class HealthController {
+
+    @Get(value = "/health")
+    public HttpResponse<String> health() {
+        return HttpResponse.ok("OK");
+    }
+}
